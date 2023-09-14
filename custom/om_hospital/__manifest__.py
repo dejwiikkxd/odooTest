d430fabd3acabb9c2160d25938e9d56fa4ebbed7
dejwiikkxd <120561006+dejwiikkxd@users.noreply.github.com>
2023-09-14 10:47:37 +0200

    1

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Hospital Management System(testovaci_modul)',
+    'author': 'dejwiikk',
+    'summary': 'Odoo development testing',
+    'data': [
+        'security/ir.model.access.csv',
+        'D:/odoo-16.0/custom/om_hospital/views/menu.xml',
+        'D:/odoo-16.0/custom/om_hospital/views/patient.xml',
+    ]
+}
